a3fd755ae17ac0647270396f7d8eb56aa49df0b8
TheGamer3514 <thegamer3514@gmail.com>
2022-12-02 20:26:08 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+#Check If Installed
+try: 
+    import requests  
+except ImportError: 
+    print("Requests Not Found...\nInstalling...")
+    os.system("pip install requests")
+    print("Requests Installed")
+#import
+import random
+import string
+import time
+import os
+import requests
+#start the gen
+print("Gamer's Nitro Generator\nDiscord: Gamer3514#7679\nIf you like this tool give it a star on github!")
+time.sleep(0.75)
+print("Starting The Gen...")
+time.sleep(2.5)
+print("Gen Started. Enjoy!")
+#gen codes
+def gencode():
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(19))
+
+class Generator:
+    def __init__(self):
+        self.codes = []
+        self.check()
+
+    def check(self):
+        while True:
+            code = gencode()
+            self.codes.append(code)
+            response = requests.get(
+                "https://discord.com/api/v7/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
+            data = response.json()
+            if data["message"] == 'Unknown Gift Code':
+                print("Invalid: " + code)
+            elif data["message"] == 'The resource is being rate limited.' or 'You are being rate limited.':
+                print('Rate Limited: ' + code)
+                print('Waiting For Rate Limit To Go Away...')
+                print(data)
+                time.sleep(int(data['retry_after'])/1000)
+            else:
+                print("Working: " + code)
+                file = open("workedcodes.txt", "a+")
+                file.write("\n" + code)
+Generator()
