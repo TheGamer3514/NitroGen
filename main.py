3f670430af11854091c9209607b301fddea2558d
Gamer3514 <gamer@sillydev.co.uk>
2024-06-05 23:34:47 +0100

    Update
    Updated branding and added some minor improvements

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ try:
     import os
     import requests  
 except ImportError: 
-    print("Requests Not Found...\nInstalling...")
     os.system("pip install requests")
-    print("Requests Installed")
+try:
+    import os
+    import gamer3514
+except ImportError:
+    os.system("pip install gamer3514")
 #import
 import random
 import string
 import time
 import os
 import requests
-web = True #Change this to False to disable the keep alive server
+from gamer3514 import randomstuff
+web = False #Change this to True to enable the keep alive server
 #start the gen
-print("Gamer's Nitro Generator\nDiscord: Gamer3514#7679\nIf you like this tool give it a star on github!")
+randomstuff.niceprint("Gamer's Nitro Generator\nDiscord: discord.gg/sillydev\nIf you like this tool give it a star on github!")
 time.sleep(0.75)
-print("Starting The Gen...")
-time.sleep(2.5)
-print("Gen Started. Enjoy!")
+randomstuff.niceprint("Starting The Gen...")
+time.sleep(0.25)
+randomstuff.niceprint("Gen Started. Enjoy!")
 #gen codes
 def gencode():
     letters = string.ascii_letters + string.digits
@@ -37,14 +41,14 @@ class Generator:
                 "https://discord.com/api/v7/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
             data = response.json()
             if data["message"] == 'Unknown Gift Code':
-                print("Invalid: " + code)
+                randomstuff.niceprint("Invalid: " + code)
             elif data["message"] == 'The resource is being rate limited.' or 'You are being rate limited.':
-                print('Rate Limited: ' + code)
-                print('Waiting For Rate Limit To Go Away...')
-                print(data)
+                randomstuff.niceprint('Rate Limited: ' + code)
+                randomstuff.niceprint('Waiting For Rate Limit To Go Away...')
+                randomstuff.niceprint(data)
                 time.sleep(int(data['retry_after'])/1000)
             else:
-                print("Working: " + code)
+                randomstuff.niceprint("Working: " + code)
                 file = open("workedcodes.txt", "a+")
                 file.write("\n" + code)
 #Keep Alive
